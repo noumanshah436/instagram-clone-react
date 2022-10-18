08c17ad512e7d4f41046e2a9956beb896d6dd177
noumanshah436 <noumanshah436@gmail.com>
2022-10-19 00:29:10 +0500

    APi completed

--- src/components/allAccounts.js
+++ src/components/allAccounts.js
@@ -1,62 +1,51 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import { Link } from 'react-router-dom'
-import image from '../assets/default.png'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import image from "../assets/default.png";
 
-// const API_URL = "http://localhost:3000/api/v1/profile";
-// const API_URL = 'https://instagram-clone-pk.herokuapp.com/api/v1/profile'
-const API_URL = process.env.REACT_APP_ALL_ACCOUNTS
 
-function getAPIData () {
-  return axios.get(API_URL).then((response) => response.data)
+function getImage(account) {
+  return account.attributes.image.url === "default.png"
+  ? image
+  : account.attributes.image.standard.url;
 }
 
-function getImage (account) {
-  return account.attributes.image.url === 'default.png'
-    ? image
-    : account.attributes.image.standard.url
-}
-
-function Accounts () {
-  const [accounts, setAccounts] = useState([])
+function Accounts() {
+  const [accounts, setAccounts] = useState([]);
+  const API_URL = process.env.REACT_APP_ALL_ACCOUNTS;
 
   useEffect(() => {
-    let mounted = true
-    getAPIData().then((items) => {
-      // console.log(items);
-      if (mounted) {
-        setAccounts(items.data)
-      }
-    })
-    return () => (mounted = false)
-  }, [])
+    axios
+      .get(API_URL)
+      .then((response) => {
+        setAccounts(response.data.data);
+      })
+
+      .catch((error) => console.log(error));
+  }, [API_URL]);
 
   return (
-    <div className='text-center'>
-      <h1 className='mt-5'>All Accounts</h1>
+    <div className="text-center">
+      <h1 className="mt-5">All Accounts</h1>
 
       {accounts.map((account) => {
         return (
-          <div key={account.id} className='item'>
+          <div key={account.id} className="item">
             {/* {console.log(account)} */}
             <img
               src={getImage(account)}
-              alt='profile_pic'
-              className='my-image '
+              alt="profile_pic"
+              className="my-image "
             />
             <h4>{account.attributes.name}</h4>
-            <Link
-              className='btn btn-primary'
-              to={`/followers/${account.id}`}
-            >
+            <Link className="btn btn-primary" to={`/followers/${account.id}`}>
               Followers
             </Link>
-
           </div>
-        )
+        );
       })}
     </div>
-  )
+  );
 }
 
-export default Accounts
+export default Accounts;
--- src/components/stories.js
+++ src/components/stories.js
@@ -1,40 +1,34 @@
-import React, { useEffect, useState } from 'react'
-import Story from './story'
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import Story from "./story";
+import axios from "axios";
 
-// const API_URL = "http://localhost:3000/api/v1/stories";
-const API_URL = process.env.REACT_APP_ALL_STORIES
 
-function getAPIData () {
-  return axios.get(API_URL).then((response) => response.data)
-}
-
-function Stories () {
-  const [stories, setStories] = useState([])
+function Stories() {
+  const [stories, setStories] = useState([]);
+  const API_URL = process.env.REACT_APP_ALL_STORIES;
 
   useEffect(() => {
-    let mounted = true
-    getAPIData().then((items) => {
-      if (mounted) {
-        setStories(items.data)
-      }
-    })
-    return () => (mounted = false)
-  }, [])
+    axios
+      .get(API_URL)
+      .then((response) => {
+        setStories(response.data.data);
+      })
+      .catch((error) => console.log(error));
+  }, [API_URL]);
 
-  console.log(stories)
+  console.log(stories);
   return (
-    <div className='text-center'>
-      <h1 className='mt-5'>All Stories</h1>
+    <div className="text-center">
+      <h1 className="mt-5">All Stories</h1>
       {stories.map((story) => {
         return (
-          <div key={story.id} className='my-5'>
+          <div key={story.id} className="my-5">
             <Story story={story} />
           </div>
-        )
+        );
       })}
     </div>
-  )
+  );
 }
 
-export default Stories
+export default Stories;
